91d9abe390e3e174de240217044ca1a0f25ce1ba
AU <>
2025-03-07 16:38:30 +0800

    re-added: entry rate

--- src/FldrPages/FldrEntry/rate.tsx
+++ src/FldrPages/FldrEntry/rate.tsx
@@ -5,12 +5,13 @@ import axios from "axios";
 import { plsConnect } from "@/FldrClass/ClsGetConnection";
 
 
+
 export default function Rate() {
   const [data, setData] = useState<RateCol[]>([]);
 
   useEffect(() => {
     axios
-      .get<RateCol[]>(`${plsConnect}/API/WEBAPI/ListController/ListRate`)
+      .get<RateCol[]>(`${plsConnect()}/API/WEBAPI/ListController/ListRate`)
       .then((response) => {
         setData(response.data);
         console.log("wtf", data);
