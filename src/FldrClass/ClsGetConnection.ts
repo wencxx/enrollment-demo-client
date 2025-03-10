cf5e7d61b20710f2ee6deb0b84a0ab218cd0647b
Kim Diaz <k.tiangjadiaz@gmail.com>
2025-03-10 14:53:20 +0800

    enrollment1

--- src/FldrClass/ClsGetConnection.ts
+++ src/FldrClass/ClsGetConnection.ts
@@ -1,6 +1,6 @@
  export const plsConnect = () => {
     // const strEndpoint: string = 'https://192.168.254.126:7092'
-    const strEndpoint: string = 'https://192.168.254.107:7092'
+    const strEndpoint: string = 'https://192.168.254.108:7092'
     // const strEndpoint: string = 'https://localhost:7092'
     return strEndpoint
 }
