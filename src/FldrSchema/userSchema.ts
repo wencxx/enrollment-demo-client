649a293b7da8e0566ba557fd2e0105c1aeef9e88
Kim Diaz <k.tiangjadiaz@gmail.com>
2025-03-12 13:31:19 +0800

    fixed: typo in login schema

--- src/FldrSchema/userSchema.ts
+++ src/FldrSchema/userSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const loginSchema = z.object({
   username: z.string({
-    required_error: "Please enter a password",
+    required_error: "Please enter a username",
   }).min(3).max(50),
   password: z.string({
     required_error: "Please enter a password",
