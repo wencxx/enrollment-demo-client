b3aceca8b4eaf94b8fd5d1fabdcfee76bb1b98b9
Kim Diaz <k.tiangjadiaz@gmail.com>
2025-05-16 11:15:18 +0800

    fixed: dynamic submenu item color

--- src/FldrPages/FldrLayout/layout.tsx
+++ src/FldrPages/FldrLayout/layout.tsx
@@ -47,18 +47,18 @@ function Layout() {
       htmlElement.classList.remove('dark');
       localStorage.setItem('theme', 'light');
       setTheme('light')
-      htmlElement.style.setProperty("--sidebar", 'oklch(0.985 0.001 106.423)');
-      localStorage.setItem('color', 'oklch(0.985 0.001 106.423)');
-      htmlElement.style.setProperty("--sidebar-foreground", 'oklch(0.147 0.004 49.25)');
-      localStorage.setItem('text', 'oklch(0.147 0.004 49.25)');
+      // htmlElement.style.setProperty("--sidebar", 'oklch(0.985 0.001 106.423)');
+      // localStorage.setItem('color', 'oklch(0.985 0.001 106.423)');
+      // htmlElement.style.setProperty("--sidebar-foreground", 'oklch(0.147 0.004 49.25)');
+      // localStorage.setItem('text', 'oklch(0.147 0.004 49.25)');
     } else {
       htmlElement.classList.add('dark');
       localStorage.setItem('theme', 'dark');
       setTheme('dark')
-      htmlElement.style.setProperty("--sidebar", 'oklch(0.216 0.006 56.043)');
-      localStorage.setItem('color', 'oklch(0.216 0.006 56.043)');
-      htmlElement.style.setProperty("--sidebar-foreground", 'oklch(0.985 0.001 106.423)');
-      localStorage.setItem('text', 'oklch(0.985 0.001 106.423)');
+      // htmlElement.style.setProperty("--sidebar", 'oklch(0.216 0.006 56.043)');
+      // localStorage.setItem('color', 'oklch(0.216 0.006 56.043)');
+      // htmlElement.style.setProperty("--sidebar-foreground", 'oklch(0.985 0.001 106.423)');
+      // localStorage.setItem('text', 'oklch(0.985 0.001 106.423)');
     }
   }
 
